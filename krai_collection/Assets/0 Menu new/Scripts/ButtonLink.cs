4557cfbc352c3450fa69e4a5c0dbe4698feb7883
vvrvvr <vover@mail.ru>
2021-11-04 17:27:54 +0300

    курсор изменяется при наведении на ссылку

--- krai_collection/Assets/0 Menu new/Scripts/ButtonLink.cs
+++ krai_collection/Assets/0 Menu new/Scripts/ButtonLink.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 public class ButtonLink : MonoBehaviour
 {
     [SerializeField] private string link;
+    public Texture2D cursor1;
     public void OpenLink()
     {
         Application.OpenURL(link);
+    }
+    public void OnMouseOver()
+    {
+        Cursor.SetCursor(cursor1, Vector2.zero, CursorMode.Auto);
+    }
+    public void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 }
