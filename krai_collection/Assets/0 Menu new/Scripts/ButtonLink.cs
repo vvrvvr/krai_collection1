1422577894c215bb01f675a475aef751d2597093
vvrvvr <vover@mail.ru>
2021-11-05 22:36:45 +0300

    меню кнопка выхода, правки авторов, глаз

--- krai_collection/Assets/0 Menu new/Scripts/ButtonLink.cs
+++ krai_collection/Assets/0 Menu new/Scripts/ButtonLink.cs
@@ -6,6 +6,8 @@ public class ButtonLink : MonoBehaviour
     public Texture2D cursor1;
     public void OpenLink()
     {
+        if (link == "") 
+            return;
         Application.OpenURL(link);
     }
     public void OnMouseOver()
@@ -15,5 +17,14 @@ public class ButtonLink : MonoBehaviour
     public void OnMouseExit()
     {
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+             Application.Quit();
+#endif
     }
 }
