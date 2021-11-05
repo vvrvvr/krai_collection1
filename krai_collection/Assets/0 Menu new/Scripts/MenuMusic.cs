78a4ea4c88726b091974ac5cd4457bad57ca8a66
vvrvvr <vover@mail.ru>
2021-11-05 23:07:23 +0300

    меню изменены звуки фмод, добавлен эмбиенс

--- krai_collection/Assets/0 Menu new/Scripts/MenuMusic.cs
+++ krai_collection/Assets/0 Menu new/Scripts/MenuMusic.cs
@@ -10,27 +10,34 @@ namespace krai_menu
     {
         [Space]
         [SerializeField] protected bool _musicOn;
+        [SerializeField] protected bool _ambienceOn;
 
         [Space]
         [SerializeField] protected float _masterVolume = 1;
         [SerializeField] protected float _musicVolume = 1;
         [SerializeField] protected float _typingVolume = 1;
         [SerializeField] protected float _messageVolume = 1;
+        [SerializeField] protected float _ambienceVolume = 1;
 
         [Space]
         //[FMODUnity.EventRef] [SerializeField] protected string _typing;
         [FMODUnity.EventRef] [SerializeField] protected string _music;
         [FMODUnity.EventRef] [SerializeField] protected string _typing;
+        [FMODUnity.EventRef] [SerializeField] protected string _ambience;
         [FMODUnity.EventRef] [SerializeField] protected string _messageOneShot;
 
         protected FMOD.Studio.EventInstance _musicEvent;
         protected FMOD.Studio.EventInstance _typingEvent;
+        protected FMOD.Studio.EventInstance _ambienceEvent;
 
 
         void Start()
         {
             if (_musicOn)
                 PlayMusic();
+            if (_ambienceOn)
+                PlayAmbience();
+
             StartTypingEvent();
 
         }
@@ -39,6 +46,7 @@ namespace krai_menu
         {
             _typingEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
             _musicEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            _ambienceEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         }
 
         //private void Update()
@@ -64,6 +72,17 @@ namespace krai_menu
             _musicEvent.start();
             _musicEvent.setParameterByName("music_fade", 1f);
         }
+
+        public void PlayAmbience()
+        {
+            if (_ambience == "") return;
+
+            _ambienceEvent = FMODUnity.RuntimeManager.CreateInstance(_ambience);
+            _ambienceEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject.transform));
+            _ambienceEvent.setVolume(_ambienceVolume * _masterVolume);
+            _ambienceEvent.start();
+        }
+
         public void StartTypingEvent()
         {
             if (_typing == "") return;
@@ -74,6 +93,7 @@ namespace krai_menu
             _typingEvent.start();
             _typingEvent.setPaused(true);
         }
+
         public void PlayTypingSound(bool isPlay)
         {
             if (isPlay)
@@ -90,6 +110,7 @@ namespace krai_menu
                 _musicEvent.setParameterByName("music_fade", 1f);
 
         }
+
         public void PlayMessageSound()
         {
             FMODUnity.RuntimeManager.PlayOneShotAttached(_messageOneShot, this.gameObject);
